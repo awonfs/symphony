bb6a4d5655287cc8bf9711019df7c7dd12176456
Eetu Rönkkö <105866472+awonfs@users.noreply.github.com>
2023-09-28 17:12:15 +0300

    Added a spinner for loading states

--- components/top-tracks.tsx
+++ components/top-tracks.tsx
@@ -1,21 +1,24 @@
 "use client";
 import { useGetTopTracks } from "@/lib/hooks/useGetTopTracks";
+import Spinner from "./ui/spinner";
 
 function TopTracks({ token }: { token: string }) {
   const { data, error, isLoading } = useGetTopTracks(token);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <Spinner />;
   if (error) return <div>Error occurred: {error.message}</div>;
 
   return (
-    <div className="flex flex-col items-start bg-purple-700 shadow-inner drop-shadow-2xl opacity-80 rounded-lg p-4 mt-4">
+    <div className="flex flex-col items-start bg-purple-700 shadow-inner drop-shadow-2xl opacity-75 rounded-lg p-4 mt-4">
       <div className="w-1/2">
         {data?.items.map((item, index) => (
           <div
             key={index}
             className="flex items-center mb-2 text-start bg-purple-600 text-lime-300 shadow-inner drop-shadow-2xl rounded-xl px-4 py-2 w-3/4 overflow-scroll"
           >
-            <div className="text-2xl font-bold mr-2">{index + 1}.</div>
+            <div className="text-3xl font-bold mr-4 drop-shadow-2xl">
+              {index + 1}
+            </div>
             <div className="flex flex-col">
               <span className=" text-lime-300 font-semibold text-xl">
                 {item.name}
---
+++ components/ui/spinner.tsx
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <div className="flex items-center justify-center min-h-screen min-w-screen">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-lime-500"></div>
+    </div>
+  );
+}
+
+export default Spinner;
