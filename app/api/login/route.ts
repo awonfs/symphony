8d15dcbe9197116053df382795e5c58101bfd0b3
Eetu Rönkkö <105866472+awonfs@users.noreply.github.com>
2023-10-03 10:37:11 +0300

    Updated spotify login state variable

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -4,7 +4,7 @@ export async function GET(): Promise<NextResponse> {
   const client_id: string = process.env.NEXT_PUBLIC_CLIENT_ID!!;
   const redirect_uri: string = process.env.NEXT_PUBLIC_REDIRECT_URI!!;
   const scope: string = "user-read-private user-read-email user-top-read";
-  const state: string = "someRandomState";
+  const state: string = "L0k1TH3C4TISR34L";
 
   const queryParams: string = new URLSearchParams({
     response_type: "code",
