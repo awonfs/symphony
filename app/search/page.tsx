56748f41c6b694c6be02dc82976bad705bc0e1d9
Eetu Rönkkö <105866472+awonfs@users.noreply.github.com>
2023-10-03 16:03:43 +0300

    Added less margin top for md screens on searchpage

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -18,7 +18,7 @@ function SearchPage() {
     );
   }
   return (
-    <div className="container flex flex-col md:w-3/4 mt-28 text-center h-screen text-white">
+    <div className="container flex flex-col md:w-3/4 mt-28 md:mt-6 text-center h-screen text-white">
       <SearchPageForm token={token.value} />
       <ArtistCardGrid />
     </div>
